7af2f55c3631cdf61279e7497e0f09c7dbd6ee98
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-02-25 22:19:57 +0200

    Create UserController.java

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,77 @@
+package com.example.controller;
+import com.example.model.Order;
+import com.example.model.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    //The Dependency Injection Variables
+    private UserService userService;
+
+    //The Constructor with the requried variables mapping the Dependency Injection.
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    public User addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/")
+    public ArrayList<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable UUID userId){
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public List<Order> getOrdersByUserId(@PathVariable UUID userId){
+        return userService.getOrdersByUserId(userId);
+    }
+
+    @PostMapping("/{userId}/checkout")
+    public String addOrderToUser(@PathVariable UUID userId){
+        return userService.addOrderToUser(userId);
+    }
+
+    @PostMapping("/{userId}/removeOrder")
+    public String removeOrderFromUser(@PathVariable UUID userId, @RequestParam UUID orderId){
+        return userService.removeOrderFromUser(userId, orderId);
+    }
+
+    @DeleteMapping("/{userId}/emptyCart")
+    public String emptyCart(@PathVariable UUID userId){
+        return userService.emptyCart(userId);
+    }
+
+    //TODO: Add addProductToCart in UserService
+    @PutMapping("/addProductToCart")
+    public String addProductToCart(@RequestParam UUID userId, @RequestParam UUID productId){
+        return userService.addProductToCart(userId, productId);
+    }
+
+    //TODO: Add deleteProductFromCart in UserService
+    @PutMapping("/deleteProductFromCart")
+    public String deleteProductFromCart(@RequestParam UUID userId, @RequestParam UUID productId){
+        return userService.deleteProductFromCart(userId, productId);
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public String deleteUserById(@PathVariable UUID userId){
+        return userService.deleteUserById(userId);
+    }
+
+}
