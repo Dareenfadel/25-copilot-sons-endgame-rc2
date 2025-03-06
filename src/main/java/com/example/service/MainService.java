3027faf018daafb7f35b936ee903e7078f16a92e
Mathew Hany <mathew.hanybb@gmail.com>
2025-03-06 11:59:53 +0000

    feat: add helper methods to MainService

--- src/main/java/com/example/service/MainService.java
+++ src/main/java/com/example/service/MainService.java
@@ -1,6 +1,5 @@
 package com.example.service;
 
-
 import com.example.repository.MainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
@@ -9,16 +8,17 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Primary
 @Service
 public abstract class MainService<T> {
 
     protected final MainRepository<T> repository; // Dependency Injection for repository
-        private final String entityType; // The entity type of the service
-
+    private final String entityType; // The entity type of the service
+    
     @Autowired
-    protected MainService(MainRepository<T> repository,  String entityType) {
+    public MainService(MainRepository<T> repository, String entityType) {
         this.repository = repository;
         this.entityType = entityType;
     }
@@ -26,7 +26,57 @@ public abstract class MainService<T> {
     // Validate if an entity exists, including class name in the error message
     protected void validateExistence(UUID id) {
         if (repository.findById(id) == null) {
-            throw new NoSuchElementException(entityType+ " not found");
+            throw new NoSuchElementException(entityType + " not found");
+        }
+    }
+    
+    // ----------------------
+    // CRUD Operations
+    // ----------------------
+    
+    public T create(T entity) {
+        return repository.create(entity);
+    }
+
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+    
+    public T findById(UUID id) {
+        return repository.findById(id);
+    }
+    
+    public T updateById(UUID id, Consumer<T> consumer) {
+        return repository.updateById(id, consumer);
+    }
+    
+    public void updateEach(Consumer<T> consumer) {
+        repository.updateEach(consumer);
+    }
+    
+    public void deleteById(UUID id) {
+        repository.deleteById(id);
+    }
+    
+    public void replaceModelsInList(List<T> list, T newModel) {
+        var newModelId = repository.getIdFromModel(newModel);
+        
+        for (int i = 0; i < list.size(); i++) {
+            var model = list.get(i);
+            var modelId = repository.getIdFromModel(model);
+            if (modelId.equals(newModelId)) {
+                list.set(i, newModel);
+            }
+        }
+    }
+    
+    public void deleteModelFromListById(List<T> list, UUID id) {
+        for (int i = 0; i < list.size(); i++) {
+            var model = list.get(i);
+            var modelId = repository.getIdFromModel(model);
+            if (modelId.equals(id)) {
+                list.remove(i);
+            }
         }
     }
 }
